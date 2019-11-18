538b400b1cf6dcdcde64bc37e0d5def08fa56b86
KaitoKudou <kaitokudou2468110921@gmail.com>
2019-11-19 01:03:36 +0900

    ボタンを押すと音が鳴る機能を追加

--- MyMusic/MyMusic/ViewController.swift
+++ MyMusic/MyMusic/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,70 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // シンバルの音源ファイルを指定
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    
+    // シンバル用のプレイヤーインスタンスを作成
+    // cymbalPlayerがシンバルを鳴らす
+    var cymbalPlayer: AVAudioPlayer!
+    
+    @IBAction func cymbal(_ sender: Any) {
+        
+        do{
+            //　シンバル用のプレイヤーに、音楽ファイル名を指定
+            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+            
+            // シンバルの音声を再生
+            cymbalPlayer.play()
+        }
+        catch{
+            print("シンバルを鳴らそうとしたけど、エラーが発生")
+        }
+    }
+    
+    // ギター用の音源ファイルを指定
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    
+    // ギター用のプレイヤーインスタンスを作成
+    var guitarPlayer : AVAudioPlayer!
+    
+    @IBAction func guitar(_ sender: Any) {
+        
+        do {
+            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
+            guitarPlayer.play()
+        }
+        catch {
+            print("ギターを鳴らそうとしたけど、エラーが発生")
+        }
+    }
+    
+    // バックミュージック用の音源ファイルを指定
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    
+    // バックミュージック用のプレイヤーインスタンスを作成
+    var backmusicPlayer: AVAudioPlayer!
+    
+    @IBAction func play(_ sender: Any) {
+        
+        do {
+            backmusicPlayer =  try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
+            backmusicPlayer.numberOfLoops = -1
+            backmusicPlayer.play()
+        }
+        catch {
+            print("エラーが発生")
+        }
+    }
+    
+    // stopボタンが押された時
+    @IBAction func stop(_ sender: Any) {
+        
+        // バックミュージックを停止
+        backmusicPlayer.stop()
+    }
+    
+    
 }
 
