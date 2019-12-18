178384b830c50aa4ea49d02629d4072695ca2a6d
KaitoKudou <kaitokudou2468110921@gmail.com>
2019-12-18 12:24:04 +0900

    検索機能実装、緯度経度表示、地図にピンを立てる機能の実装

--- MyMap/MyMap/ViewController.swift
+++ MyMap/MyMap/ViewController.swift
@@ -31,10 +31,50 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // 入力された文字列を取り出す
         if let searchKey = textField.text{
             print(searchKey)
+            
+            let geocoder = CLGeocoder(); // CLGeocoderインスタンスを生成
+            
+            // 入力された文字から位置情報を取得
+            geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks, error) in
+                
+                // 位置情報が尊意するときは、unwrapPlacemarksに取り出す
+                if let unwrapPlacemarks = placemarks{
+                    
+                    // 1件目を取り出す
+                    if let firstPlacemark = unwrapPlacemarks.first{
+                        
+                        // 位置情報を取り出す
+                        // locationには、経度・緯度、高度などの情報が入っている
+                        if let location = firstPlacemark.location{
+                            
+                            // 位置情報から経度・緯度をtargetCoordinateに取り出す
+                            // coordinateには、経度・緯度が格納されている
+                            let targetCoodinate = location.coordinate
+                            
+                            //　経度・緯度をでバックエリアに表示
+                            print(targetCoodinate)
+                            
+                            // MKPointAnnotationインスタンスを生成し、ピンを作成
+                            let pin = MKPointAnnotation();
+                            
+                            // ピンの置く場所に経度・緯度を設定
+                            pin.coordinate = targetCoodinate
+                            
+                            // ピンにつけるタイトルを設定
+                            pin.title = searchKey
+                            
+                            // ピンを地図に置く
+                            self.dispMap.addAnnotation(pin)
+                            
+                            // 経度・緯度を中心として、半径500mの範囲を表示
+                            self.dispMap.region = MKCoordinateRegion(center: targetCoodinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0)
+                        }
+                    }
+                }
+            })
         }
-        
         return true
     }
-    
 }
+        
 
