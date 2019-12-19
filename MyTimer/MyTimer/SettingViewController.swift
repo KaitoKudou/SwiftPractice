9e514dcad6fe77b6fb4bfd982fdf93d8714b9d7c
KaitoKudou <kaitokudou2468110921@gmail.com>
2019-12-19 17:46:22 +0900

    Label, Button, PickerViewをコードと紐付けた

--- MyTimer/MyTimer/SettingViewController.swift
+++ MyTimer/MyTimer/SettingViewController.swift
@@ -27,4 +27,10 @@ class SettingViewController: UIViewController {
     }
     */
 
+    @IBOutlet weak var timerSettingPicker: UIPickerView!
+    
+    
+    @IBAction func decisionButtonAction(_ sender: Any) {
+    }
+    
 }
