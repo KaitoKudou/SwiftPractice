a1290e812e53e8ee3f32c683169050fc9bb16ccf
KaitoKudou <kaitokudou2468110921@gmail.com>
2019-12-25 18:09:16 +0900

    タイマー画面のカウントダウン機能とストップ機能を追加

--- MyTimer/MyTimer/ViewController.swift
+++ MyTimer/MyTimer/ViewController.swift
@@ -9,21 +9,115 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // タイマーの変数を作成
+    // Timerクラスのインスタンス
+    var timer : Timer?
+    
+    // 経過時間の変数を作成
+    var count = 0
+    
+    // 設定値を扱うキーを設定
+    // 秒数を保持する
+    let settingKey = "timer_value"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // UserDefaultsインスタンスを生成
+        let settings = UserDefaults.standard
+        
+        // UserDefaultsに初期値を設定
+        settings.register(defaults: [settingKey:10])
+    }
+    
+    // 画面切り替えのタイミングで処理を行う
+    // タイマー画面が表示される度に実行される
+    override func viewDidAppear(_ animated: Bool) {
+        
+         // カウント(経過時間)を0にする
+        count = 0
+        
+        // タイマー表示を更新する
+        _ = displayUpdate()
     }
 
     @IBOutlet weak var countDownLabel: UILabel!
     
     @IBAction func settingButtonAction(_ sender: Any) {
+        // timerをアンラップしてnowTimerに格納
+        if let nowTimer = timer {
+            
+            // もしタイマーが実行中だったら停止
+            if nowTimer.isValid == true {
+                
+                // タイマー停止
+                nowTimer.invalidate()
+            }
+        }
+        
+        // 画面遷移を行う
+        performSegue(withIdentifier: "goSetting", sender: nil)
     }
     
     @IBAction func startButtonAction(_ sender: Any) {
+        // timerをアンラップしてnowTimerに格納
+        if let nowTimer = timer {
+            
+            // もしタイマーが実行中だったらスタートしない
+            if nowTimer.isValid == true {
+                return // 何もしない
+            }
+        }
+        
+        // タイマーをスタート
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerInterrupt(_:)), userInfo: nil, repeats: true)
     }
     
     @IBAction func stopButtonAction(_ sender: Any) {
+         // timerをアンラップしてnowTimerに格納
+        // アンラップしてtimerに値が存在する時、タイマーを停止
+        if let nowTimer = timer {
+            
+            // もしタイマーが、実行中だったら停止
+            if nowTimer.isValid == true {
+                 // タイマーを停止
+                nowTimer.invalidate()
+            }
+        }
+    }
+    
+    // 画面の更新をする（戻り値：remainCount：残り時間）
+    func displayUpdate() -> Int{
+        // UserDefaultsインスタンスを生成
+        let settings = UserDefaults.standard
+        
+        // 取得した秒数をtimerValueに格納
+        let timerValue = settings.integer(forKey: settingKey)
+        
+        // 残り時間(remainCount)を生成
+        let remainCount = timerValue - count
+        
+        // remainCountをcountDownLabelに反映
+        countDownLabel.text = "残り\(remainCount)秒"
+        
+        return remainCount
+    }
+    
+    // 経過時間の処理
+    @objc func timerInterrupt(_ timer:Timer){
+        // 経過時間を足す
+        count += 1
+        
+        // displayUpdate()が0以下の時、タイマーを止める
+        if displayUpdate() <= 0 {
+            // 初期化し処理
+            count = 0
+            
+            // タイマー停止
+            timer.invalidate()
+        }
     }
     
 }
