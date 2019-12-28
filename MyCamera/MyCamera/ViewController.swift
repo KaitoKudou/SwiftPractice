2f942b614c922af3d5ac269dcd6bdb5bafcb6eb9
KaitoKudou <kaitokudou2468110921@gmail.com>
2019-12-28 13:14:23 +0900

    ButtonとUIImageViewを配置して、コードと紐付け完了

---
+++ MyCamera/MyCamera/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyCamera
+//
+//  Created by KaitoKudou on 2019/12/28.
+//  Copyright © 2019 KaitoKudou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var pictureImage: UIImageView!
+    
+    @IBAction func cameraButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func shareButtonAction(_ sender: Any) {
+    }
+    
+}
+
