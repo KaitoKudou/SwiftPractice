0e49c57bced5b1f45463ad53378e9344080761fa
工藤海斗 <kaitokudou2468110921@gmail.com>
2020-01-10 15:25:08 +0900

    UIパーツ配置完了

---
+++ MyOkashi/MyOkashi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by 工藤海斗 on 2020/01/10.
+//  Copyright © 2020 工藤海斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
